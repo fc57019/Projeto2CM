ef8125e50fd8029053936b382e1460dc4492bafe
fc57019 <fc57019@alunos.fc.ul.pt>
2021-01-14 20:41:33 +0000

    API google

--- app/src/main/java/com/example/projeto2cm/activities/MainActivity.kt
+++ app/src/main/java/com/example/projeto2cm/activities/MainActivity.kt
@@ -1,15 +1,41 @@
 package com.example.projeto2cm.activities
 
 import android.content.Intent
+import android.content.IntentSender.SendIntentException
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageButton
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.projeto2cm.R
 import com.example.projeto2cm.utils.NavigationManager
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.Scopes
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.common.api.ResultCallback
+import com.google.android.gms.common.api.Scope
+import com.google.android.gms.fitness.Fitness
+import com.google.android.gms.fitness.data.DataPoint
+import com.google.android.gms.fitness.data.DataSource
+import com.google.android.gms.fitness.data.DataType
+import com.google.android.gms.fitness.data.Value
+import com.google.android.gms.fitness.request.DataSourcesRequest
+import com.google.android.gms.fitness.request.OnDataPointListener
+import com.google.android.gms.fitness.request.SensorRequest
+import com.google.android.gms.fitness.result.DataSourcesResult
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
+import java.util.concurrent.TimeUnit
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), OnDataPointListener, GoogleApiClient.ConnectionCallbacks,
+    GoogleApiClient.OnConnectionFailedListener {
+
+    private val REQUEST_OAUTH = 1
+    private var count: Int = 0
+    private val AUTH_PENDING = "auth_state_pending"
+    private var authInProgress = false
+    private var mApiClient: GoogleApiClient? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +44,14 @@ class MainActivity : AppCompatActivity() {
         if (!screenRotated(savedInstanceState)) {
             NavigationManager.goToHomeFragment(supportFragmentManager)
         }
+
+        mApiClient = GoogleApiClient.Builder(this)
+            .addApi(Fitness.SENSORS_API)
+            .addScope(Scope(Scopes.FITNESS_ACTIVITY_READ_WRITE))
+            .addScope(Scope(Scopes.FITNESS_LOCATION_READ_WRITE))
+            .addConnectionCallbacks(this)
+            .addOnConnectionFailedListener(this)
+            .build()
 
         val bottomNavigationView =
             findViewById<BottomNavigationView>(R.id.bottom_navigation)?.setOnNavigationItemSelectedListener {
@@ -48,12 +82,129 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onStart() {
+        super.onStart()
+        mApiClient!!.connect()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Fitness.SensorsApi.remove(mApiClient, this)
+            .setResultCallback { status ->
+                if (status.isSuccess) {
+                    mApiClient!!.disconnect()
+                }
+            }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBoolean(AUTH_PENDING, authInProgress)
+    }
+
     override fun onBackPressed() {
         moveTaskToBack(true)
     }
 
     private fun screenRotated(savedInstanceState: Bundle?): Boolean {
         return savedInstanceState != null
+    }
+
+    override fun onDataPoint(p0: DataPoint) {
+        for (field in p0.dataType.fields) {
+            val value: Value = p0.getValue(field)
+            runOnUiThread {
+                Toast.makeText(
+                    applicationContext,
+                    "Field: " + field.name.toString() + " Value: " + value,
+                    Toast.LENGTH_SHORT
+                ).show()
+                Log.e("111111", "Field: " + field.name.toString() + " Value: " + value)
+            }
+        }
+    }
+
+    override fun onConnected(p0: Bundle?) {
+        val dataSourceRequest = DataSourcesRequest.Builder()
+            .setDataTypes(DataType.TYPE_STEP_COUNT_CUMULATIVE)
+            .setDataSourceTypes(DataSource.TYPE_RAW)
+            .build()
+
+        val dataSourcesResultCallback =
+            ResultCallback<DataSourcesResult> { dataSourcesResult ->
+                for (dataSource in dataSourcesResult.dataSources) {
+                    if (DataType.TYPE_STEP_COUNT_CUMULATIVE == dataSource.dataType) {
+                        registerFitnessDataListener(dataSource, DataType.TYPE_STEP_COUNT_CUMULATIVE)
+                    }
+                }
+            }
+
+        Fitness.SensorsApi.findDataSources(mApiClient, dataSourceRequest)
+            .setResultCallback(dataSourcesResultCallback)
+
+        val dataSourceRequest1 = DataSourcesRequest.Builder()
+            .setDataTypes(DataType.TYPE_DISTANCE_DELTA)
+            .setDataSourceTypes(DataSource.TYPE_RAW)
+            .build()
+
+        val dataSourcesResultCallback1 =
+            ResultCallback<DataSourcesResult> { dataSourcesResult ->
+                for (dataSource in dataSourcesResult.dataSources) {
+                    if (DataType.TYPE_DISTANCE_DELTA == dataSource.dataType) {
+                        registerFitnessDataListener(dataSource, DataType.TYPE_DISTANCE_DELTA)
+                    }
+                }
+            }
+
+        Fitness.SensorsApi.findDataSources(mApiClient, dataSourceRequest1)
+            .setResultCallback(dataSourcesResultCallback1)
+    }
+
+    private fun registerFitnessDataListener(dataSource: DataSource, dataType: DataType) {
+        val request = SensorRequest.Builder()
+            .setDataSource(dataSource)
+            .setDataType(dataType)
+            .setSamplingRate(3, TimeUnit.SECONDS)
+            .build()
+
+        Fitness.SensorsApi.add(mApiClient, request, this)
+            .setResultCallback { status ->
+                if (status.isSuccess) {
+                    Log.e("GoogleFit", "SensorApi successfully added")
+                }
+            }
+    }
+
+    override fun onConnectionSuspended(p0: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onConnectionFailed(p0: ConnectionResult) {
+        if (!authInProgress) {
+            try {
+                authInProgress = true
+                p0.startResolutionForResult(this@MainActivity, REQUEST_OAUTH)
+            } catch (e: SendIntentException) {
+            }
+        } else {
+            Log.e("GoogleFit", "authInProgress")
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_OAUTH) {
+            authInProgress = false
+            if (resultCode == RESULT_OK) {
+                if (!mApiClient!!.isConnecting && !mApiClient!!.isConnected) {
+                    mApiClient!!.connect()
+                }
+            } else if (resultCode == RESULT_CANCELED) {
+                Log.e("GoogleFit", "RESULT_CANCELED")
+            }
+        } else {
+            Log.e("GoogleFit", "requestCode NOT request_oauth")
+        }
     }
 
 }
